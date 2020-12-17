a48c158008fd3bcf9d262b6e7039c47374ebe0ee
olavast <76154028+olavast@users.noreply.github.com>
2020-12-17 11:14:59 +0100

    Update edit-user.js

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -13,28 +13,28 @@ class EditUser extends LitElement {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   </head>
   <form onsubmit="javascript: return false;" id="userForm" method="POST">
-  <div class="form-group" style="width: 40rem;">
+  <div class="form-group" style="width: 30rem;">
     <label for="email">Email</label>
     <input class="form-control" id="uname" name="uname" type="text" value="${this.user.uname}" required>
     <input type="hidden" id="uid" name="uid" value="${this.user.uid}">
   </div>
-  <div class="form-group" style="width: 40rem;">
-    <label for="firstName">Fornavn</label>
-    <input class="form-control" id="firstName" name="firstName" type="text" value="${this.user.firstName}" required>
-  </div>
-  <div class="form-group" style="width: 40rem;">
+  <div class="form-group" style="width: 30rem;">
     <label for="lastName">Etternavn</label>
     <input class="form-control" id="lastName" name="lastName" type="text" value="${this.user.lastName}" required>
   </div>
-  <div class="form-group" style="width: 40rem;">
+  <div class="form-group" style="width: 30rem;">
+    <label for="firstName">Fornavn</label>
+    <input class="form-control" id="firstName" name="firstName" type="text" value="${this.user.firstName}" required>
+  </div>
+  <div class="form-group" style="width: 30rem;">
     <label for="oldpwd">Gammelt Passord</label>
     <input type="password" class="form-control" id="oldpwd" name="oldpwd" type="text" value="">
   </div>
-  <div class="form-group" style="width: 40rem;">
+  <div class="form-group" style="width: 30rem;">
     <label for="newpwd">Nytt Passord</label>
     <input type="password" class="form-control" id="pwd" name="pwd" type="text" value="">
 </div>
-<input type="submit" @click=${this.updateUser} id="submitForm" name="editUser" class="btn btn-success mt-4 ml-2" value="Edit User"></input>
+<input type="submit" @click=${this.updateUser} id="submitForm" name="editUser" class="btn btn-success mt-4 ml-2" value="Endre Info"></input>
 </form>
   `;
 }
@@ -49,9 +49,9 @@ updateUser(e) {
   }).then(res=>res.json())
     .then(data=>{
       if (data.status=='success') {
-          console.log("The user was updated");
+          console.log("Brukeren ble oppdatert");
       } else {
-          console.log("The user was not updated");
+          console.log("Brukeren ble ikke oppdatert");
       }
     })
 }
